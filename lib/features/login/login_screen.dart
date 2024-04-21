4e0d4fc2436f6e8c2ef6e32983fecf2033f88aeb
ezriouil <ezriouil2003@gmail.com>
2024-04-21 20:28:27 +0100

    setup project structure

---
+++ lib/features/login/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:grocery_admin_panel/utils/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:grocery_admin_panel/utils/responsive/responsive.dart';
+import 'package:flutter/services.dart';
+
+class MainDashboardScreen extends Responsive {
+  const MainDashboardScreen({super.key});
+
+  @override
+  Widget execute(BuildContext context) {
+
+    // - - - - - - - CHANGE THEME OF TOP STATUS BAR AND SYSTEM BOTTOM BAR - - - - - - - - -  //
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: isDark(context) ? CustomColors.BLACK : CustomColors.WHITE,
+      statusBarIconBrightness: isDark(context) ? Brightness.light : Brightness.dark,
+      statusBarBrightness: isDark(context) ? Brightness.light : Brightness.dark,
+      systemNavigationBarColor: isDark(context) ? CustomColors.BLACK : CustomColors.WHITE,
+      systemNavigationBarIconBrightness: isDark(context) ? Brightness.light : Brightness.dark,
+    ));
+
+    return Container(color: darkDarkLightLightColor(context));
+  }
+}
