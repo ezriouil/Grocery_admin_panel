aadb209a562a2048645be1f0436017474749dc61
ezriouil <ezriouil2003@gmail.com>
2024-04-21 21:52:31 +0100

    Setup project colors.

--- lib/utils/constants/custom_colors.dart
+++ lib/utils/constants/custom_colors.dart
@@ -3,25 +3,35 @@ import 'package:flutter/material.dart';
 class CustomColors{
   CustomColors._();
 
-  /*________________________________________*/
-  static const WHITE= Colors.white;
-  static const BLACK = Colors.black;
+  /*__________________ PRIMARY COLOR ______________________*/
+  static const PRIMARY_LIGHT = Color(0xFF002E94);
+  static const PRIMARY_DARK = Color(0xFF5E91FF);
 
-  /*________________________________________*/
-  static const PRIMARY_DARK = Color(0xFFCA2422);
-  static const PRIMARY_LIGHT = Color(0xD3C83126);
-
-  /*________________________________________*/
+  /*___________________ BASIC COLORS _____________________*/
+  static const WHITE = Color(0xFFFFFFFF);
+  static const BLACK = Color(0xFF000000);
   static const TRANSPARENT = Color(0x00000000);
-  static const ORANGE = Color(0xD3FF8700);
-  static const YELLOW = Color(0xD3FFDD00);
-  static const GREEN = Color(0xD00CBD4E);
-  static const BLUE = Color(0xFF18429D);
 
-/*________________________________________*/
-  static const RED_DARK = Color(0xFFCA2422);
-  static const RED_LIGHT = Color(0xD3C83126);
-  static const GRAY_LIGHT= Color(0xFFC0C5D0);
-  static const GRAY_DARK= Color(0xFF738290);
+/*__________________ COLORS ______________________*/
+  static const GRAY_LIGHT = Color(0xFFB8B8C2);
+  static const GRAY_DARK = Color(0xFF64717C);
+
+  static const RED_LIGHT = Color(0xFFFF0200);
+  static const RED_DARK = Color(0xFF880200);
+
+  static const PURPLE_LIGHT= Color(0xFF7460C5);
+  static const PURPLE_DARK = Color(0xFF3F0686);
+
+  static const BLUE_LIGHT = Color(0xFF0097FF);
+  static const BLUE_DARK = Color(0xFF121481);
+
+  static const GREEN_LIGHT = Color(0xFFEFFF00);
+  static const GREEN_DARK = Color(0xFFFFC94A);
+
+  static const YELLOW_LIGHT = Color(0xD000FF60);
+  static const YELLOW_DARK = Color(0xFF004225);
+
+  static const ORANGE_LIGHT = Color(0xFFFC6736);
+  static const ORANGE_DARK = Color(0xFF8F4600);
 
 }
