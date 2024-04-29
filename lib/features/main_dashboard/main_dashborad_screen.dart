88c40d596829af00acb419ce591f5af633634566
ezriouil <ezriouil2003@gmail.com>
2024-04-29 22:46:16 +0100

    fix some bugs in notification screen + controller

--- lib/features/main_dashboard/main_dashborad_screen.dart
+++ lib/features/main_dashboard/main_dashborad_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:grocery_admin_panel/features/main_dashboard/main_dashborad_controller.dart';
 import 'package:grocery_admin_panel/features/main_dashboard/widgets/custom_main_dashboard_card.dart';
-import 'package:grocery_admin_panel/utils/constants/custom_colors.dart';
-=======
 import 'package:grocery_admin_panel/utils/constants/custom_sizes.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../utils/state/custom_state.dart';
@@ -21,87 +19,18 @@ class MainDashboardScreen extends CustomState {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal : CustomSizes.SPACE_BETWEEN_ITEMS / 3),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-                const SizedBox(height: CustomSizes.SPACE_BETWEEN_SECTIONS * 2),
-
-                // - - - - - - - - - - - - - - - - - - IMG + TITLE + SUBTITLE - - - - - - - - - - - - - - - - - -  //
-                Row(
-                  children: [
-
-                    // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-                    const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS),
-
-                    Container(width: 50, height: 50,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(CustomSizes.SPACE_BETWEEN_ITEMS), border: Border.all(color: darkLightColor(context)), color: darkDarkLightLightColor(context)),
-                        child: Icon(Iconsax.user, color: darkLightColor(context))
-                    ),
-
-                    // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-                    const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS),
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Mohamed Ezriouil", style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold, letterSpacing: 1)),
-                        Text("welcome back,", style: Theme.of(context).textTheme.bodySmall),
-                      ],
-                    ),
-
-                  ],
-                ),
-
-                // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-                const SizedBox(height: CustomSizes.SPACE_BETWEEN_SECTIONS),
-
-                // - - - - - - - - - - - - - - - - - - STORES + COMMANDS - - - - - - - - - - - - - - - - - -  //
-                Row(
-                  children: [
-                    Expanded(
-                    child: CustomMainDashboardCard(
-                        title: "Stores",
-                        subTitle:
-                            "Here You Can Manage Your Stores & Adding new Stores ",
-                        icon: Iconsax.shop,
-                        onClick: controller.onNavigateToStoreScreen)),
-                    const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
-                    Expanded(
-                    child: CustomMainDashboardCard(
-                        title: "Commands",
-                        subTitle:
-                            "Here You Can Manage Your Stores & Adding new Stores ",
-                        icon: Iconsax.activity,
-                        onClick: controller.onNavigateToCommandScreen)),
-              ],
-            ),
-
-                // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-
-                const SizedBox(height: CustomSizes.SPACE_BETWEEN_ITEMS),
-
-                // - - - - - - - - - - - - - - - - - -  DELIVERIES + DELIVERIES - - - - - - - - - - - - - - - - - -  //
-          Row(
-              children: [
-                Expanded(child: CustomMainDashboardCard(title: "Deliveries", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.truck, onClick: controller.onNavigateToDeliveryScreen)),
-                const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
-                Expanded(child: CustomMainDashboardCard(title: "Settings", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.setting, onClick: controller.onNavigateToSettingsScreen)),
         padding: const EdgeInsets.symmetric(horizontal : CustomSizes.SPACE_BETWEEN_ITEMS / 2),
         child: SingleChildScrollView(
           child: Column(children: [
 
             // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
             const SizedBox(height: CustomSizes.SPACE_BETWEEN_SECTIONS * 2),
-          
+
             // - - - - - - - - - - - - - - - - - - STORES + PRODUCTS - - - - - - - - - - - - - - - - - -  //
             Row(
               children: [
-                 Expanded(child: CustomMainDashboardCard(title: "Stores", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.shop, onClick: controller.onNavigateToStoreScreen)),
-                 const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
+                Expanded(child: CustomMainDashboardCard(title: "Stores", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.shop, onClick: controller.onNavigateToStoreScreen)),
+                const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
                 Expanded(child: CustomMainDashboardCard(title: "Products", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.activity, onClick: controller.onNavigateToProductScreen)),
               ],
             ),
@@ -131,16 +60,10 @@ class MainDashboardScreen extends CustomState {
             ),
 
             // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-            const SizedBox(height: CustomSizes.SPACE_BETWEEN_SECTIONS * 2),
-
             const SizedBox(height: CustomSizes.SPACE_DEFAULT),
 
             // - - - - - - - - - - - - - - - - - -  VERSION  - - - - - - - - - - - - - - - - - -  //
             Center(child: Text("version 1.0.0", style: Theme.of(context).textTheme.bodySmall)),
-
-                // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-                const SizedBox(height: CustomSizes.SPACE_BETWEEN_SECTIONS * 2),
-
 
             // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
             const SizedBox(height: CustomSizes.SPACE_BETWEEN_ITEMS),
