b14ab116a72f69de551c70cb5615936ebdd25608
ezriouil <ezriouil2003@gmail.com>
2024-04-29 23:09:44 +0100

    fix some bugs

--- lib/features/main_dashboard/main_dashborad_screen.dart
+++ lib/features/main_dashboard/main_dashborad_screen.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:grocery_admin_panel/features/main_dashboard/main_dashborad_controller.dart';
 import 'package:grocery_admin_panel/features/main_dashboard/widgets/custom_main_dashboard_card.dart';
 import 'package:grocery_admin_panel/utils/constants/custom_sizes.dart';
 import 'package:iconsax/iconsax.dart';
+
 import '../../utils/state/custom_state.dart';
 
 class MainDashboardScreen extends CustomState {
@@ -31,7 +30,7 @@ class MainDashboardScreen extends CustomState {
               children: [
                 Expanded(child: CustomMainDashboardCard(title: "Stores", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.shop, onClick: controller.onNavigateToStoreScreen)),
                 const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
-                Expanded(child: CustomMainDashboardCard(title: "Products", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.activity, onClick: controller.onNavigateToProductScreen)),
+                Expanded(child: CustomMainDashboardCard(title: "Commands", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.activity, onClick: controller.onNavigateToProductScreen)),
               ],
             ),
 
@@ -43,19 +42,7 @@ class MainDashboardScreen extends CustomState {
               children: [
                 Expanded(child: CustomMainDashboardCard(title: "Deliveries", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.truck, onClick: controller.onNavigateToDeliveryScreen)),
                 const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
-                Expanded(child: CustomMainDashboardCard(title: "Orders", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.command, onClick: controller.onNavigateToCommandScreen)),
-              ],
-            ),
-
-            // - - - - - - - - - - - - - - - - - - SPACER - - - - - - - - - - - - - - - - - -  //
-            const SizedBox(height: CustomSizes.SPACE_BETWEEN_ITEMS / 2),
-
-            // - - - - - - - - - - - - - - - - - - SETTINGS + MORE - - - - - - - - - - - - - - - - - -  //
-            Row(
-              children: [
                 Expanded(child: CustomMainDashboardCard(title: "Settings", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.setting, onClick: controller.onNavigateToSettingsScreen)),
-                const SizedBox(width: CustomSizes.SPACE_BETWEEN_ITEMS / 4),
-                Expanded(child: CustomMainDashboardCard(title: "More", subTitle: "Here You Can Manage Your Stores & Adding new Stores ", icon: Iconsax.more, onClick: controller.onNavigateToMoreScreen)),
               ],
             ),
 
