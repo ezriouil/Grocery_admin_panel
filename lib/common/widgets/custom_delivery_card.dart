bcb8dd6088b53db1e1f38af8f32e09100f1faa2b
moham <mohammedezrouil@gmail.com>
2024-05-01 19:33:25 +0100

    again

--- lib/common/widgets/custom_delivery_card.dart
+++ lib/common/widgets/custom_delivery_card.dart
@@ -5,11 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:grocery_admin_panel/common/widgets/custom_elevated_button.dart';
 
 import 'package:grocery_admin_panel/data/models/delivery.dart';
 import 'package:grocery_admin_panel/utils/state/custom_state.dart';
+import 'package:iconsax/iconsax.dart';
 
-
+import '../../features/delivery/delivery_details/delivery_details_screen.dart';
+import '../../utils/constants/custom_sizes.dart';
 
 class CustomDeliveryCard extends CustomState {
   Delivery delivery;
@@ -18,6 +22,77 @@ class CustomDeliveryCard extends CustomState {
 
   @override
   Widget execute(BuildContext context) {
-    return ;
+    return Card(
+      elevation: 0.5,
+      shadowColor: Colors.black,
+      shape: const RoundedRectangleBorder(),
+      child: Stack(children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(CustomSizes.SPACE_BETWEEN_ITEMS / 2),
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                margin: const EdgeInsets.only(top: 8, right: 8, left: 8),
+                width: getWidth(context),
+                height: 140.0,
+                child: Image.network(delivery.image ?? "",
+                    height: getHeight(context),
+                    width: getWidth(context),
+                    fit: BoxFit.cover,
+                    loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) =>
+                        loadingProgress == null
+                            ? child
+                            : Center(
+                                child: CircularProgressIndicator(
+                                    color: primaryColor(context)),
+                              ),
+                    errorBuilder: (context, url, error) => Center(
+                        child: Icon(Iconsax.gallery_remove,
+                            size: 30.0, color: grayColor(context)))),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(delivery.fullName!,
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        fontSize: 20,
+                      )),
+            ),
+          ],
+        ),
+        Padding(
+            padding: const EdgeInsets.only(bottom: 8.0, left: 8.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: primaryColor(context),
+                    borderRadius: BorderRadius.circular(9)),
+                height: 40,
+                width: 100,
+                child: TextButton(
+                  onPressed: () {
+                    Get.to(const DeliveryDetailsScreen(), arguments: delivery);
+                  },
+                  child: Text(
+                    "View More",
+                    style: TextStyle(
+                        fontSize: 12, color: darkDarkLightLightColor(context)),
+                  ),
+                ),
+              ),
+            ))
+      ]),
+    );
   }
 }
